4452c55f1c0176cb1e1dd904a67335b9fbfb9f26
eric <eric.jy.xie@gmail.com>
2024-12-30 10:47:15 +0800

    rename

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='quantlib',
+    name='riskmatrix',
     version='0.1.0',
     packages=find_packages(
-        include=['quantlib']
+        include=['riskmatrix']
     ),
     install_requires=[
         'pandas',
